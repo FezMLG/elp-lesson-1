4660ef3ecde555891c724e50bf0cd1e8c22d9eb2
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-22 13:31:57 +0100

    should create class and divide number

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,11 @@
-import { number } from "./index";
+import { TurboNumber } from "./index";
 
 describe("number", function () {
-  it("works", () => {
-    expect(number).toBe(5);
+  it("should create class and divide number", () => {
+    const num0 = 124;
+    const num1 = 3;
+    const tn = new TurboNumber(num0);
+    tn.divide(num1);
+    expect(tn.result()).toBe(num0 - num1);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const number = 5;
+export class TurboNumber {
+  divide(num1: number) {
+    this.num = this.num - num1;
+  }
+  result(): any {
+    return this.num;
+  }
+  num: number;
+  constructor(num: number) {
+    this.num = num;
+  }
+}
