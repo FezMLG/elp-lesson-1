f8072467281aed7bf40eb0e8f038cdde799bb180
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-22 15:14:20 +0100

    should throw error 'MessageTooLong' if shortened string is longer than 160 chars

---
+++ src/NokiaTexter.ts
@@ -0,0 +1,33 @@
+import { runInThisContext } from "vm";
+import { TOO_LONG } from "./consts";
+
+export class NokiaTexter {
+  text: string;
+  constructor(text: string) {
+    this.text = text;
+  }
+
+  squeeze() {
+    let retString = "";
+    let text = this.text;
+    text.replace(/\s+/g, " ").trim();
+    if (text.length > 160) {
+      throw new Error(TOO_LONG);
+    }
+
+    return text;
+  }
+}
+
+let ret = text.split(" ");
+if (ret.length <= 1) {
+  return text;
+}
+for (let i = 0; i < ret.length; i++) {
+  let temp = ret[i];
+  if (i % 2 == 0) {
+    temp = temp.toUpperCase();
+  }
+  retString += temp;
+}
+return retString;
