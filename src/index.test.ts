1422deeefefe865da59031d0f5fd1adeab25ce44
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-22 13:18:53 +0100

    adding beforeeach

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,27 +1,25 @@
 import { TurboNumber } from "./index";
 
 describe("number", function () {
+  const num0 = 10;
+  const num1 = 2;
+  const num2 = 4;
+  let tn: TurboNumber;
+
+  beforeEach(() => {
+    tn = new TurboNumber(num0);
+  });
   it("should create class and substract numbers", () => {
-    const num0 = 10;
-    const num1 = 2;
-    const tn = new TurboNumber(num0);
     tn.substract(num1);
     expect(tn.result()).toBe(num0 - num1);
   });
 
   it("should create class and divide numbers", () => {
-    const num0 = 10;
-    const num1 = 2;
-    const tn = new TurboNumber(num0);
     tn.divide(num1);
     expect(tn.result()).toBe(num0 / num1);
   });
 
   it("should create class and divide and substract numbers", () => {
-    const num0 = 10;
-    const num1 = 2;
-    const num2 = 4;
-    const tn = new TurboNumber(num0);
     tn.divide(num1);
     tn.substract(num2);
     expect(tn.result()).toBe(num0 / num1 - num2);
