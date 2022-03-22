0a952c16e8069641bf05e9f49096538208a553fc
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-22 11:04:43 +0100

    creating class in beforeach

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,56 +2,49 @@ import { DIVIDE_BY_ZERO_ERROR } from "./consts";
 import { TurboNumber } from "./index";
 
 describe("number", function () {
+  let tn: TurboNumber;
+  const num0 = 17;
+  beforeEach(() => {
+    tn = new TurboNumber(num0);
+  });
   it("subtracts 2 numbers", () => {
-    const num1 = 12;
     const num2 = 5;
-    const a = new TurboNumber(num1);
-    a.subtract(num2);
-    expect(a.result()).toBe(num1 - num2);
+    tn.subtract(num2);
+    expect(tn.result()).toBe(num0 - num2);
   });
 
   it("devides 2 numbers", () => {
-    const num1 = 12;
     const num2 = 5;
-    const a = new TurboNumber(num1);
-    a.divide(num2);
-    expect(a.result()).toBe(num1 / num2);
+    tn.divide(num2);
+    expect(tn.result()).toBe(num0 / num2);
   });
 
   it("subtracts 3 numbers", () => {
-    const num1 = 12;
     const num2 = 5;
     const num3 = 3;
-    const a = new TurboNumber(num1);
-    a.subtract(num2);
-    expect(a.result()).toBe(num1 - num2);
-    a.subtract(num3);
-    expect(a.result()).toBe(num1 - num2 - num3);
+    tn.subtract(num2);
+    expect(tn.result()).toBe(num0 - num2);
+    tn.subtract(num3);
+    expect(tn.result()).toBe(num0 - num2 - num3);
   });
 
   it("devides 3 numbers", () => {
-    const num1 = 12;
     const num2 = 5;
     const num3 = 3;
-    const a = new TurboNumber(num1);
-    a.divide(num2);
-    a.divide(num3);
-    expect(a.result()).toBe(num1 / num2 / num3);
+    tn.divide(num2);
+    tn.divide(num3);
+    expect(tn.result()).toBe(num0 / num2 / num3);
   });
 
   it("expect to throw error when dividing by 0", () => {
-    const num1 = 12;
     const num2 = 0;
-    const a = new TurboNumber(num1);
-    expect(() => a.divide(num2)).toThrow(DIVIDE_BY_ZERO_ERROR);
+    expect(() => tn.divide(num2)).toThrow(DIVIDE_BY_ZERO_ERROR);
   });
 
   it("possible to run function from function", () => {
-    const num0 = 17;
     const num1 = 5;
     const num2 = -3;
     const num3 = -4;
-    const a = new TurboNumber(num0);
-    expect(a.subtract(num1).divide(num2).result()).toBe(num3);
+    expect(tn.subtract(num0).divide(num2).result()).toBe(num3);
   });
 });
