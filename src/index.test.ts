e30b490211afea834494cd472ba2ba865070bea8
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-22 13:34:15 +0100

    should create class and subtract and divide number

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,4 +16,14 @@ describe("number", function () {
     tn.subtract(num1);
     expect(tn.result()).toBe(num0 / num1);
   });
+
+  it("should create class and subtract and divide number", () => {
+    const num0 = 124;
+    const num1 = 3;
+    const num2 = 5;
+    const tn = new TurboNumber(num0);
+    tn.subtract(num1);
+    tn.divide(num2);
+    expect(tn.result()).toBe(num0 / num1 - num2);
+  });
 });
