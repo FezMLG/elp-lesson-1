474bbe0091fd0c7d3792b2b3538e7f2a510fb064
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-23 14:43:55 +0100

    small refactor

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,22 @@
 import { TurboNumber } from "./index";
 
 describe("number", function () {
+  const num0 = 20;
+  const num1 = 10;
+  const num2 = 5;
   it("should correctly divide numbers", () => {
-    const tn = new TurboNumber(10);
+    const tn = new TurboNumber(num0);
 
-    tn.divide(5);
+    tn.divide(num1);
 
-    expect(tn.result()).toBe(10 / 5);
+    expect(tn.result()).toBe(num0 / num1);
   });
 
   it("corretly subtract numbers", () => {
-    const tn = new TurboNumber(10);
+    const tn = new TurboNumber(num0);
 
-    tn.subtract(5);
+    tn.subtract(num1);
 
-    expect(tn.result()).toBe(10 - 5);
+    expect(tn.result()).toBe(num0 - num1);
   });
 });
