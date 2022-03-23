fcf09e8ab1bea09f1bd9d5441d1c39a6d98a1c16
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-03-23 14:55:15 +0100

    refactoring and adding beforeeach

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,31 +4,27 @@ describe("number", function () {
   const num0 = 20;
   const num1 = 10;
   const num2 = 5;
+  let tn: TurboNumber;
+  beforeEach(() => {
+    tn = new TurboNumber(num0);
+  });
   it("should correctly divide numbers", () => {
-    const tn = new TurboNumber(num0);
-
     tn.divide(num1);
 
     expect(tn.result()).toBe(num0 / num1);
   });
 
   it("corretly subtract numbers", () => {
-    const tn = new TurboNumber(num0);
-
     tn.subtract(num1);
 
     expect(tn.result()).toBe(num0 - num1);
   });
 
   it("corretly throws errow when try to divide by 0.", () => {
-    const tn = new TurboNumber(num0);
-
     expect(() => tn.divide(0)).toThrow(ZERO_ERROR);
   });
 
   it("should allow chaining", () => {
-    const tn = new TurboNumber(num0);
-
     expect(tn.subtract(num1).divide(num2).result()).toBe((num0 - num1) / num2);
   });
 });
